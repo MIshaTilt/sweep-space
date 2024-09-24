c64fb6d521e042505fa37630270a859dd195eedb
Михаил Свинаренко <mdsv@inbox.ru>
2024-09-24 19:21:54 +0300

    mech done

---
+++ void-nexus/Assets/FutuRift/FutuRiftHandler.cs
@@ -0,0 +1,94 @@
+#region
+
+using ChairControl.ChairWork;
+using ChairControl.ChairWork.Options;
+using Unity.VisualScripting;
+using UnityEngine;
+
+#endregion
+
+namespace Futurift
+{
+    public class FutuRiftHandler : MonoBehaviour
+    {
+        private FutuRiftController _futuRiftController;
+        public Rigidbody playerRigidbody;
+
+        public float tiltSensitivity = 1.0f;  // Чувствительность наклона
+        public float rollSensitivity = 1.0f;  // Чувствительность крена
+
+        public Transform targTrans;
+
+        private Transform lastPosition;         // Предыдущее положение игрока
+
+        private Transform lastTransform;
+
+        private float tilt; // Наклон
+        private float roll; // Крен
+
+
+        void Start()
+        {
+            // Сохраняем начальную позицию игрока
+            lastPosition = targTrans;
+
+            lastTransform = new GameObject().transform;
+            lastTransform.position = transform.position;
+            lastTransform.rotation = transform.rotation;
+
+        }
+
+
+        private void Update()
+        {
+            //Vector3 deltaPosition = transform.position - lastTransform.position;
+
+            // Получаем направление движения
+            //Vector3 direction = deltaPosition.normalized;
+
+            // Вычисляем наклон (tilt) относительно оси X (вверх-вниз) и крен (roll) относительно оси Z (влево-вправо)
+            //float tilt = Mathf.Atan2(direction.y, direction.z) * Mathf.Rad2Deg * tiltSensitivity;
+            //float roll = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg * rollSensitivity;
+
+            // Получаем ускорение Rigidbody
+            Vector3 acceleration = playerRigidbody.velocity / Time.deltaTime;
+
+            // Преобразуем ускорение в наклон и крен
+            // Наклон будет соотноситься с осью X, крен — с осью Z
+            //tilt = Mathf.Clamp(acceleration.y, -1f, 1f); // Ограничиваем наклон от -1 до 1
+            //roll = Mathf.Clamp(acceleration.x, -1f, 1f); // Ограничиваем крен от -1 до 1
+
+            tilt = acceleration.z * tiltSensitivity;
+            roll = acceleration.x * rollSensitivity;
+
+            //var euler = transform.eulerAngles;
+            _futuRiftController.Pitch = -(tilt > 180 ? tilt - 360 : tilt);
+            _futuRiftController.Roll = -(roll > 180 ? roll - 360 : roll);
+            Debug.Log($"Pitch: {RoundToDecimalPlaces(_futuRiftController.Pitch, 2)}, Roll: {RoundToDecimalPlaces(_futuRiftController.Roll, 2)},");
+        }
+
+        public float RoundToDecimalPlaces(float value, int decimalPlaces)
+        {
+            float scale = Mathf.Pow(10f, decimalPlaces);
+            return Mathf.Round(value * scale) / scale;
+        }
+
+        private void OnEnable()
+        {
+            _futuRiftController = new  FutuRiftController(
+                new UdpOptions
+                {
+                    // ip = "192.168.50.126",  // ip компьютера, на котором запущен контроллер (не локальный)
+                    ip= "127.0.0.1",            // докальный
+                    port = 6065             // порт, на который настроен контролле
+                });
+
+            _futuRiftController.Start();
+        }
+
+        private void OnDisable()
+        {
+            _futuRiftController.Stop();
+        }
+    }
+}
---
+++ void-nexus/Assets/Scripts/physicsHead.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class physicsHead : MonoBehaviour
+{
+
+
+    [SerializeField] private GameObject head;
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float rotateSpeed = 100f;
+
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Vector3 rotationOffset;
+
+
+    private Transform _followTarget;
+    private Rigidbody _body;
+
+    private void Start()
+    {
+        _followTarget = head.transform;
+        _body = GetComponent<Rigidbody>();
+        _body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        _body.interpolation = RigidbodyInterpolation.Interpolate;
+        _body.mass = 20f;
+        _body.maxAngularVelocity = 20f;
+
+        _body.position = _followTarget.position;
+        _body.rotation = _followTarget.rotation;
+    }
+
+    void FixedUpdate()
+    {
+        PhysicsMove();
+
+        Gizmos.DrawWireSphere(positionOffset, 1);
+    }
+
+    private void PhysicsMove()
+    {
+        var positionWithOffset = _followTarget.TransformPoint(positionOffset);
+        var distance = Vector3.Distance(positionWithOffset, transform.position);
+        _body.velocity = (positionWithOffset - transform.position).normalized * (followSpeed * distance);
+
+        var rotationWithOffset = _followTarget.rotation * Quaternion.Euler(rotationOffset);
+        var q = rotationWithOffset * Quaternion.Inverse(_body.rotation);
+        q.ToAngleAxis(out float angle, out Vector3 axis);
+        _body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
+    }
+
+}
